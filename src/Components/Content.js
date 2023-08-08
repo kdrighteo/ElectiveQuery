7180ae16caf20a5d07a905574537e9aaf4e5bb58
Kdrighteo <kdrighteo@gmail.com>
2023-08-08 12:31:51 +0000

    qb

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -5,8 +5,8 @@ export default function Content() {
     <div className=" flex justify-center m-8">
       <div className="text-l max-w-prose text-center text-l font-mono	">
         <p>
-          Welcome to ElectiveQuery, Please Enter your username/ID and Choose
-          your department to access this Query
+          Welcome to ElectiveQuery, Please Enter your ID and Choose your
+          department to access this Query
         </p>
         <p className=" mt-4">
           Ref: 1071212 <br />
